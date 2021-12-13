45ae5e7daa067e99405916d2c269d6caba5182b7
9thprince <castpo84@gmail.com>
2021-12-13 14:53:03 +0000

    Update host_matrix.py

--- host_matrix.py
+++ host_matrix.py
@@ -30,11 +30,11 @@ fncs_search = ['showsearch','myshowsearchmovie','myshowsearchserie','showmoviess
 
 def getinfo():	
     info_={}
-    info_['name']='matrix'
+    info_['name']='Matrix'
     info_['version']='1.0 11/11/2019'
     info_['dev']='RGYSOFT'
     info_['cat_id']='902'
-    info_['desc']='matrix (KODI Addon)'
+    info_['desc']='Matrix (KODI Addon)'
     info_['icon']='https://i.ibb.co/wJ5k47d/icon.png'
     return info_
     
@@ -58,7 +58,7 @@ def get_url_meta(URL):
 
 def getHosts():	
     Hosts=[]
-    Hosts.append(('26','LIVETV','livetv'             ,'1.0 18/01/2021','Live Sports'              ,'New Host',''))	
+#   Hosts.append(('26','LIVETV','livetv'             ,'1.0 18/01/2021','Live Sports'              ,'New Host',''))	
 #    Hosts.append(('26','','adkami_com'         ,'1.0 18/01/2021','Animés, Mangas & Séries'  ,'New Host',''))	
 #    Hosts.append(('26','','animecomplet'       ,'1.0 18/01/2021','Series & Anime'           ,'New Host',''))
     
